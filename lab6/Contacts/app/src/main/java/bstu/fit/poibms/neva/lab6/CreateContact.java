cc3a4d41ab240f166b9ea6b1a6e6794d5f8698cc
Vitaliy <vitaliy.filippov.2012@mail.ru>
2019-10-07 00:43:55 +0300

    Create three activities

--- lab6/Contacts/app/src/main/java/bstu/fit/poibms/neva/lab6/CreateContact.java
+++ lab6/Contacts/app/src/main/java/bstu/fit/poibms/neva/lab6/CreateContact.java
@@ -3,12 +3,21 @@ package bstu.fit.poibms.neva.lab6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class EnterInfo extends AppCompatActivity {
+public class CreateContact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter_info);
+        setContentView(R.layout.activity_create_contact);
+    }
+
+    public void onClickButtonWriteA(View view){
+
+    }
+
+    public void  onClickButtonWriteB(View view){
+
     }
 }
---
+++ lab6/Contacts/app/src/main/java/bstu/fit/poibms/neva/lab6/SearchInfo.java
@@ -0,0 +1,14 @@
+package bstu.fit.poibms.neva.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_info);
+    }
+}
