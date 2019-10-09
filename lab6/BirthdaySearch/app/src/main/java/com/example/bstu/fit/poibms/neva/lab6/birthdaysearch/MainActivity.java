c73098c509f2fc7f62a35b268e3cf046d8a899d1
Nickolay Yarmolik <acaroshen@gmail.com>
2019-10-09 16:01:22 +0300

    BirthdaySearch application create

---
+++ lab6/BirthdaySearch/app/src/main/java/com/example/bstu/fit/poibms/neva/lab6/birthdaysearch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bstu.fit.poibms.neva.lab6.birthdaysearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
